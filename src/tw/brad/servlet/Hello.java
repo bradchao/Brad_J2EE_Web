6b60e2714c3ed6526a9b4907cf2968095eaa5bb4
Brad Chao <brad@brad.tw>
2016-12-24 08:03:41 +0800

    2016122401

--- src/tw/brad/servlet/Hello.java
+++ src/tw/brad/servlet/Hello.java
@@ -1,6 +1,10 @@
 package tw.brad.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -23,12 +27,51 @@ public class Hello extends HttpServlet {
         System.out.println("Hello()");
     }
 
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+		System.out.println("init()");
+    		super.init(config);
+    		
+    		Enumeration<String> parameters = config.getInitParameterNames();
+    		while (parameters.hasMoreElements()){
+    			String pname = parameters.nextElement();
+    			System.out.println(pname + ":" + config.getInitParameter(pname));
+    		}
+    		
+    }
+    
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) 
+    			throws ServletException, IOException {
+        System.out.println("service()");
+    		
+    		super.service(request, response);
+    		
+    		
+    }
+    
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+        System.out.println("doGet()");
+		
+		response.setContentType("text/html; charset=UTF-8");
+		
+		PrintWriter out = response.getWriter();
+		String name = request.getParameter("name");
+		if (name != null){
+			out.print("Hello, " + name);
+		}else{
+			out.print("Hello, World");
+		}
+		
+		out.println("<br />");
+		out.println(request.getServletPath());
+		
 	}
 
 	/**
@@ -36,7 +79,8 @@ public class Hello extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+//		doGet(request, response);
+        System.out.println("doPost()");
 	}
 
 }
